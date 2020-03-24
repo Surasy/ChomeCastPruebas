5b777044915ffe1587aca92fae559cc8ecd90b77
Surasy <56151990+Surasy@users.noreply.github.com>
2020-03-24 14:43:46 +0100

    actualizacion con link de youtube

--- pruebas.py
+++ pruebas.py
@@ -1,10 +1,41 @@
 import time
 import pychromecast
+from pychromecast.controllers.youtube import YouTubeController
+
+
+def init():
+
+    chromecasts = pychromecast.get_chromecasts()
+    [cc.device.friendly_name for cc in chromecasts]
+
+    cast = next(cc for cc in chromecasts if cc.device.friendly_name == "Dormitorio")
+    cast.wait()
+    #print(cast.device) # Comprobar que se ha conectado bien
+    #print(cast.status) 
+
+    return cast
+
+
+def playVideo(cast,link):
+
+    mc = cast.media_controller
+    mc.play_media(link, 'video/mp4')
+    mc.block_until_active()
+    mc.play()
+
+def subirVolumen():
+
+
+
 
 def main() :
 
-    print('holi')
+    cast =  init()
+    yt = YouTubeController()
+    cast.register_handler(yt)
+    yt.play_video('wDgQdr8ZkTw')
+   
 
-
-
+    
+    
 main()
